e3e9c6cb0987a0bb498a9f7a80b37ffcda49b251
Infom <dsfsd>
2022-06-02 07:14:40 +0600

    Added and deleted tasks for ToDo-list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
+import Task from "./Task/Task";
+import AddTaskForm from "./Task/AddTaskForm";
 import { nanoid } from 'nanoid';
 
 
 const App = () => {
     const [tasks, setTask] = useState([
-        {text: 'Do homework', id: '1'},
-        {text: 'Cook eat', id: '2'},
-        {text: 'Go to bed', id: '3'},
+        {text: 'Buy milk', id: '1'},
+        {text: 'Walk with a dog', id: '2'},
+        {text: 'Do homework', id: '3'},
     ]);
 
     const [newTask, setNewTask] = useState([
@@ -21,9 +23,13 @@ const App = () => {
         newText.id = nanoid();
         newTaskCopy[0] = newText;
         setNewTask(newTaskCopy);
+
+
     };
 
-
+    const removeTask = id => {
+        setTask(tasks.filter(task => task.id !== id));
+    };
 
     const addTask = () => {
         const tasksList = [...tasks];
@@ -33,15 +39,27 @@ const App = () => {
         setTask(tasksList);
     }
 
+    let peopleTask = tasks.map((tasks, index) => {
+            return (
+                <Task
+                    key={index}
+                    text={tasks.text}
+                    remove={() => removeTask(tasks.id)}>
+                </Task>
+            )
+        }
+    );
     return (
         <div className="App">
+            <AddTaskForm text={tasks.text}
                          onTextChange = {event => changeTask(event)}
                          addTask = {() => addTask()} >
-
+            </AddTaskForm>
+            <div className='tasksBlock'>
+                {peopleTask}
+            </div>
         </div>
     );
 };
-
-
 
 export default App;
